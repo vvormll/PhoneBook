375b4b41d94ef987d1af8b4117cd2f246d248995
vvormll <vvormll@gmail.com>
2016-09-17 12:20:02 +0300

    Implement FIND functionality

--- src/edu/phonebook/web/ViewServlet.java
+++ src/edu/phonebook/web/ViewServlet.java
@@ -1,6 +1,9 @@
 package edu.phonebook.web;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -27,11 +30,48 @@ public class ViewServlet extends HttpServlet {
             long accountId = (long) session.getAttribute("accountId");
             ServletContext ctx = getServletContext();
             DatabaseController controller = DatabaseService.getControllerFromServletContext(ctx);
-            List<Record> records = controller.getAllRecords(accountId);
+            List<Record> records = null;
+            if (request.getParameter("act") == null) {
+                records = controller.getAllRecords(accountId);
+            } else if (request.getParameter("act").equals("find")) {
+                Map<String, String> fields = parseRequestParameters(request);
+                records = controller.getRecordsByFields(accountId, fields);
+            }
             request.setAttribute("records", records);
             RequestDispatcher disp = ctx.getRequestDispatcher("/view.jsp");
             disp.forward(request, response);
         }
+    }
+
+    private Map<String, String> parseRequestParameters(HttpServletRequest request) {
+        Map<String, String> fields = new HashMap<>();
+        Enumeration<String> en = request.getParameterNames();
+        while (en.hasMoreElements()) {
+            String paramName = en.nextElement();
+            String paramValue = request.getParameter(paramName);
+            if (paramValue == null || paramValue.isEmpty())
+                continue;
+            String dbParamName;
+            try {
+                dbParamName = translateParamToDb(paramName);
+                fields.put(dbParamName, paramValue);
+            } catch (IllegalArgumentException e) {
+                // ignore invalid fields
+                continue;
+            }
+        }
+        return fields;
+    }
+
+    private String translateParamToDb(String param) {
+        if (param.equals("name"))
+            return "contact_name";
+        else if (param.equals("number"))
+            return "number";
+        else if (param.equals("addr"))
+            return "address";
+        else
+            throw new IllegalArgumentException();
     }
 
     @Override
